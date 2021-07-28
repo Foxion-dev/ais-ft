7191252cec8acf8e1ae08c4098305cbe0a40fd7e
FoxDev <bogdahn.sazonoff@yandex.ru>
2021-07-28 17:04:02 +0300

    add adaptive

--- assets/es6/main.js
+++ assets/es6/main.js
@@ -21,38 +21,45 @@ let dayTableCountSteps          = Math.ceil((dayTableWidth - 1000) / dayTableDef
 let courseUserListMore          = document.querySelector('.js-more-users')
 
 const defaultDayOfMounthHeight = 160;
+const mediaQuery = window.matchMedia('(max-width: 1024px)')
 
-buttonsDetailCourse.forEach((elem) => {
-	elem.addEventListener('mouseover', function(event)  {
-		try{
-			let openItem = [...document.querySelectorAll('.event-info')].filter(info => info.classList.contains('open'))[0]
-			if( openItem != undefined) openItem.classList.remove('open')
+// console.log(window.clientWidth);
+if(mediaQuery.matches){
+	buttonsDetailCourse.forEach((elem) => {
+		elem.addEventListener('click', function(event)  {
+			try{
+				let openItem = [...document.querySelectorAll('.event-info')].filter(info => info.classList.contains('open'))[0]
+				if( openItem != undefined) openItem.classList.remove('open')
+	
+				this.closest('.events-list__item').querySelector('.event-info').classList.add('open')
+				let items = [...document.querySelectorAll('.calendar-table__row')].forEach(element => {
+					element.classList.remove('open')
+				});
+				this.closest('.calendar-table__row').classList.add('open')
+	
+			}catch (error){
+				console.warn(error)
+			}
+	
+		})
+	});
 
-			this.closest('.events-list__item').querySelector('.event-info').classList.add('open')
-
-		}catch (error){
-			console.warn(error)
-		}
-
-	})
-	elem.addEventListener('mouseout', function(event)  {
-		// try{
-		// 	this.closest('.events-list__item').querySelector('.event-info').classList.remove('open')
-		// }catch (error){
-		// 	console.warn(error)
-		// }
-		// try{
-		// 	this.classList.remove('full')
-
-		// 	this.querySelector('.js-hide-preview').classList.remove('hide')
-		// 	this.querySelector('.js-open-detail').classList.remove('show')
-
-		// }catch (error){
-		// 	console.warn(error)
-		// }
-
-	})
-});
+}else{
+	buttonsDetailCourse.forEach((elem) => {
+		elem.addEventListener('mouseover', function(event)  {
+			try{
+				let openItem = [...document.querySelectorAll('.event-info')].filter(info => info.classList.contains('open'))[0]
+				if( openItem != undefined) openItem.classList.remove('open')
+	
+				this.closest('.events-list__item').querySelector('.event-info').classList.add('open')
+	
+			}catch (error){
+				console.warn(error)
+			}
+	
+		})
+	});
+}
 
 buttonsDetailCourseClose.forEach((elem) => {
 	elem.addEventListener('click', function(event)  {
@@ -100,32 +107,51 @@ tabViewButtons.forEach((elem) => {
 	})
 })
 
-hoverDetailTextTrigger.forEach((elem) => {
-	elem.addEventListener('mouseover', function(event)  {
-		event.preventDefault();
-		try{
-			console.log(this);
-			this.classList.add('full')
-			this.querySelector('.js-hide-preview').classList.add('hide')
-			this.querySelector('.js-open-detail').classList.add('show')
+if(mediaQuery.matches){
+	hoverDetailTextTrigger.forEach((elem) => {
+		elem.addEventListener('click', function(event)  {
+			event.preventDefault();
+			try{
+				console.log(this);
+				this.classList.add('full')
+				this.querySelector('.js-hide-preview').classList.add('hide')
+				this.querySelector('.js-open-detail').classList.add('show')
+	
+			}catch (error){
+				console.warn(error)
+			}
+		})
 
-		}catch (error){
-			console.warn(error)
-		}
 	})
-	elem.addEventListener('mouseout', function(event)  {
-		event.preventDefault();
-		try{
-			this.classList.remove('full')
-
-			this.querySelector('.js-hide-preview').classList.remove('hide')
-			this.querySelector('.js-open-detail').classList.remove('show')
-
-		}catch (error){
-			console.warn(error)
-		}
+}else{
+	hoverDetailTextTrigger.forEach((elem) => {
+		elem.addEventListener('mouseover', function(event)  {
+			event.preventDefault();
+			try{
+				console.log(this);
+				this.classList.add('full')
+				this.querySelector('.js-hide-preview').classList.add('hide')
+				this.querySelector('.js-open-detail').classList.add('show')
+	
+			}catch (error){
+				console.warn(error)
+			}
+		})
+		elem.addEventListener('mouseout', function(event)  {
+			event.preventDefault();
+			try{
+				this.classList.remove('full')
+	
+				this.querySelector('.js-hide-preview').classList.remove('hide')
+				this.querySelector('.js-open-detail').classList.remove('show')
+	
+			}catch (error){
+				console.warn(error)
+			}
+		})
 	})
-})
+
+}
 
 
 dayTableArrowRight.addEventListener('click', function() {
@@ -170,17 +196,23 @@ dayTableArrowLeft.addEventListener('click', function() {
 	}
 })
 
+if(courseUserListMore){
+	courseUserListMore.addEventListener('click', function() {
 
-courseUserListMore.addEventListener('click', function() {
+		try{
+			this.closest('.user-list__list').querySelector('.user-list__hide-list').classList.toggle('open')
+			this.classList.toggle('active')
+			if(this.classList.contains('active')){
+				this.innerText = 'Свернуть '
+			}else{
+				this.innerText = 'Все слушатели'
+			} 
 
-	this.closest('.user-list__list').querySelector('.user-list__hide-list').classList.toggle('open')
-	this.classList.toggle('active')
-	if(this.classList.contains('active')){
-		this.innerText = 'Свернуть '
-	}else{
-		this.innerText = 'Все слушатели'
-	}
-})
+		}catch(error){
+			console.warn(error)
+		}
+	})
+}
 
 document.addEventListener('click', function(event) {
 	let openDay = [...document.querySelectorAll('.calendar-table__item')];
@@ -197,7 +229,6 @@ document.addEventListener('click', function(event) {
 		console.warn(error);
 	}
 
-	// console.log(event.target);
 })
 
 });
